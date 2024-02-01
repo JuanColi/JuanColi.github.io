911bb770fa3162b5563ddd474d7f8156138d7865
JuanColi <148549935+JuanColi@users.noreply.github.com>
2024-02-01 13:18:16 +0100

    Add files via upload

---
+++ html/Portfolio/Scripts/main.js
@@ -0,0 +1,7 @@
+const colorModeButton = document.querySelector("#colorModeButton");
+
+colorModeButton.addEventListener("click", ()=>{
+
+    document.documentElement.classList.toggle("darkMode");
+
+})
